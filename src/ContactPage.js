f557a84e141bacf80579d95c21e9177e5d6fcbc3
mahmad2005 <43645785+mahmad2005@users.noreply.github.com>
2025-06-06 00:53:17 -0600

    Fix broken 'Our Model' links

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -234,7 +234,7 @@ export default function ContactPage() {
               <li><Link to="/">Home</Link></li>
               <li><Link to="/about">About Us</Link></li>
               <li><Link to="/services">Services</Link></li>
-              <li><Link to="/model">Our Model</Link></li>
+              <li><Link to="/ourmodel">Our Model</Link></li>
               <li><Link to="/projects">Projects</Link></li>
               <li><Link to="/careers">Careers</Link></li>
               <li><Link to="/contact">Contact</Link></li>
